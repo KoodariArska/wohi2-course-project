ea17a3f76622c57ba00114c57bab5954dedd63aa
KoodariArska <arttu.kasurinen99@gmail.com>
2026-03-31 17:25:09 +0300

    created test data

---
+++ src/data/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    id: 1,
+    question: "What software company is headquartered in Redmond, Washington?",
+    answer: "Microsoft",
+    keywords: ["business"]
+  },
+  {
+    id: 2,
+    question: "What phone company produced the 3310?",
+    answer: "Nokia",
+    keywords: ["business"]
+  },
+  {
+    id: 3,
+    question: "What is the world's largest retailer as of 2026?",
+    answer: "Walmart",
+    keywords: ["retail"]
+  },
+  {
+    id: 4,
+    question: "What is a word, phrase, number, or other sequence of characters that reads the same backward as forward?",
+    answer: "Palindrome",
+    keywords: ["words"]
+  },
+];
+
+
+module.exports = questions;
